f61d12357f8a6bc10ba707ee96fcc6b6fb802d15
MoisesRM32 <141669746+MoisesRM32@users.noreply.github.com>
2024-01-30 22:25:09 -0800

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,39 +1,42 @@
 import java.io.IOException;
 import java.net.URI;
 
-
-
 class Handler implements URLHandler {
-    
-    String JoeServerText = "JOES CHAT SERVER :)";
-    // doesn't work
-
-    public String handleRequest(URI url){
-        if (url.getPath().equals("/")){
-            return JoeServerText;
-
-        }else if (url.getPath().equals("/add-message?s= %d &user= %s")){
-
-            return String.format("s=%d, user=%s") + "/n";
-        }
-        return "404 Not Found";
-    }
-
-    class ChatServer{
-        
-        public static void main(String[] args) throws IOException{
-            if (args.length == 0){
-                System.out.println("Input port number.");
-                return;
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    String Message;
+    String Username;
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Number: %d", num);
+            
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if(parameters[0].equals("s")){
+                    Message = parameters[1].split("&")[0];
+                    Username = parameters[2];
+                    return Username + ": " + Message;
+                }
             }
-        
-            int port = Integer.parseInt(args[0]);
-
-            Server.start(port, new Handler());
-
-
-        
+            return "404 Not Found!";
+        }
     }
-
 }
+
+class ChatServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
 }
